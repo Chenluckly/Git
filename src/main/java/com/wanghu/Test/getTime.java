6024ac94b9630cea0e70d93ed6ac650f1936bcb8
wanghu <2583661719@qq.com>
2022-02-14 18:14:52 +0800

    test commit message hot-fix2

--- src/main/java/com/wanghu/Test/getTime.java
+++ src/main/java/com/wanghu/Test/getTime.java
@@ -13,5 +13,6 @@ public class getTime {
         System.out.println("second");
         System.out.println("third");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
