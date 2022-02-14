2c2636858b4a1f63706ad5afcb30fa208b67f26f
wanghu <2583661719@qq.com>
2022-02-14 18:00:00 +0800

    test commit message

---
+++ src/main/java/com/wanghu/Test/getTime.java
@@ -0,0 +1,14 @@
+package com.wanghu.Test;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author wanghu
+ * @date 2022/2/14 17:55
+ */
+public class getTime {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
